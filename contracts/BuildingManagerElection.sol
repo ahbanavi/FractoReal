e14644fa09ee553cabc53f551e3f6a7091dde424
Amirhossein Banavi <ahbanavi@gmail.com>
2024-03-02 14:13:48 +0330

    Refactor building manager election contract

--- contracts/BuildingManagerElection.sol
+++ contracts/BuildingManagerElection.sol
@@ -45,7 +45,6 @@ abstract contract BuildingManagerElection {
 
     event BuildingManagerElected(address newBuildingManager);
 
-
     enum ElectionState {
         NotStarted,
         acceptCandidates,
@@ -86,6 +85,12 @@ abstract contract BuildingManagerElection {
         return _erc721TokenCount(owner) > 0;
     }
 
+    /**
+     * Registers a candidate for the building manager election.
+     * Only callable when the election state is set to acceptCandidates.
+     * The caller must own an FRN (ERC-721) token.
+     * Emits a {CandidateRegistered} event upon successful registration.
+     */
     function registerCandidate() public noContract {
         ElectionState electionState = _electionState;
 
@@ -128,36 +133,64 @@ abstract contract BuildingManagerElection {
         emit StateChanged(ElectionState.Voting);
     }
 
+    /**
+     * Allows a resident or unit owner to cast a vote for a candidate.
+     * @param tokenId The token ID representing the resident or unit owner.
+     * @param candidate_ The address of the candidate being voted for.
+     * @notice Only residents or unit owners can cast votes.
+     * @notice The voting must be in the "Voting" state.
+     * @notice The voting must not have ended.
+     * @notice The voter must not have already voted.
+     * @notice The candidate must be registered.
+     * @notice Emits a {Voted} event with the token ID, voter's address, and candidate's address.
+     */
     function castVote(
         uint256 tokenId,
         address candidate_
     ) public onlyResidentOrUnitOwner(tokenId) {
+        // Get the current election state and check if the election state is not in the "Voting" state
         ElectionState electionState = _electionState;
         if (electionState != ElectionState.Voting)
             revert InvalidStatus(electionState, ElectionState.Voting);
+
+        // Check if the voting has ended
         if (block.timestamp > votingEnd) revert VotingHasEnded();
 
+        // Get the address of the voter and check if the voter has already voted
         address voter = voters[tokenId];
-
         if (voter != address(0)) revert AlreadyVoted(voter);
 
+        // Check if the candidate is registered
         Candidate storage candidate = candidates[candidate_];
         if (!candidate.isRegistered) revert CandidateNotRegistered();
 
+        // Increment the vote count for the candidate
         ++candidate.voteCount;
 
+        // Update the voter's address in the voters mapping for preventing double voting
         voters[tokenId] = msg.sender;
 
+        // Emit a Voted event with the token ID, voter's address, and candidate's address
         emit Voted(tokenId, msg.sender, candidate_);
     }
 
+    /**
+     * Finalizes the election and determines the winning candidate as the building manager.
+     * This function can only be called when the election is in the "Voting" state and the voting period has ended.
+     * It finds the candidate with the most votes and sets them as the building manager.
+     * After setting the building manager, it resets the voting state, clears the candidate list, and resets the voter vote count.
+     * Finally, it emits the {StateChanged} and {BuildingManagerElected} events.
+     */
     function finalizeElection() public {
+        // Get the current election state and check if the election state is not in the "Voting" state
         ElectionState electionState = _electionState;
         if (electionState != ElectionState.Voting)
             revert InvalidStatus(electionState, ElectionState.Voting);
+
+        // Check if the voting has ended
         if (block.timestamp < votingEnd) revert VotingNotEnded();
 
-        // find the candidate with the most votes
+        // Find the candidate with the most votes
         uint256 maxVotes;
         Candidate storage winningCandidate = candidates[_candidateList[0]];
         uint256 length = _candidateList.length;
@@ -176,11 +209,13 @@ abstract contract BuildingManagerElection {
         }
         address winner = winningCandidate.candidate;
 
+        // Set the winning candidate as the building manager
         setBuildingManager(winner);
 
         // Reset voting state
         _electionState = ElectionState.NotStarted;
 
+        // Clear candidate mapping and list
         for (uint256 i; i != length; ) {
             delete candidates[_candidateList[i]];
             unchecked {
@@ -190,7 +225,7 @@ abstract contract BuildingManagerElection {
 
         delete _candidateList;
 
-        // also reset voterVoteCount
+        // reset voters mapping for each token ID
         length = getErc721().totalSupply();
         for (uint256 i; i != length; ) {
             delete voters[i];
@@ -199,6 +234,7 @@ abstract contract BuildingManagerElection {
             }
         }
 
+        // reset voting end timestamp
         votingEnd = 0;
 
         emit StateChanged(ElectionState.NotStarted);
